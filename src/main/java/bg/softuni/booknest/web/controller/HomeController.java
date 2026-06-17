4d5cbe1d767821019d59c1cd43a432d38cef957c
Oleksandr Sidorov <o.sidorov@tinqin.com>
2026-06-17 11:58:19 +0300

    Extracted fragments from home.html

--- src/main/java/bg/softuni/booknest/web/controller/HomeController.java
+++ src/main/java/bg/softuni/booknest/web/controller/HomeController.java
@@ -1,10 +1,18 @@
 package bg.softuni.booknest.web.controller;
 
+import bg.softuni.booknest.service.BookService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
+
+    private final BookService bookService;
+
+    public HomeController(BookService bookService) {
+        this.bookService = bookService;
+    }
 
     @GetMapping("/404")
     public String notFound() {
@@ -12,7 +20,11 @@ public class HomeController {
     }
 
     @GetMapping("/")
-    public String viewHomePage() {
-        return "home";
+    public ModelAndView viewHomePage() {
+        ModelAndView mv = new ModelAndView("home");
+
+        mv.addObject("books", bookService.getAllBooks());
+
+        return mv;
     }
 }
