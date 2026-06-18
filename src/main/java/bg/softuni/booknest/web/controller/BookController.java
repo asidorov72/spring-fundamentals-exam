fef88e579731d3142e24ef627e0b709370ae3487
Oleksandr Sidorov <o.sidorov@tinqin.com>
2026-06-18 19:22:41 +0300

    Implemented book details

--- src/main/java/bg/softuni/booknest/web/controller/BookController.java
+++ src/main/java/bg/softuni/booknest/web/controller/BookController.java
@@ -1,11 +1,17 @@
 package bg.softuni.booknest.web.controller;
 
+import bg.softuni.booknest.model.dto.BookDto;
 import bg.softuni.booknest.service.BookService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.UUID;
+
 @Controller
+@RequestMapping("/books")
 public class BookController {
 
     private final BookService bookService;
@@ -14,13 +20,24 @@ public class BookController {
         this.bookService = bookService;
     }
 
-    @GetMapping("/books")
+    @GetMapping
     public ModelAndView viewBooksPage() {
         ModelAndView mv = new ModelAndView("books");
 
         mv.addObject("activePage", "books");
-
         mv.addObject("books", bookService.getAllBooks());
+
+        return mv;
+    }
+
+    @GetMapping("/{id}")
+    public ModelAndView viewBookDetails(@PathVariable UUID id) {
+        BookDto book = bookService.getBookById(id);
+
+        ModelAndView mv = new ModelAndView("book-details");
+
+        mv.addObject("book", book);
+        mv.addObject("activePage", "books");
 
         return mv;
     }
